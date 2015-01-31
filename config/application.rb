f047e19a3c13cfde15716478e3822ebcb1ce08d0
ChangHoon Jeong <iamseapy@gmail.com>
2015-02-01 02:41:30 +0900

    Change active record schema_format :ruby to :sql
    for pg full text search index

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Lobsters
     config.action_controller.action_on_unpermitted_parameters = :raise
 
     config.cache_store = :file_store, "#{config.root}/tmp/cache/"
+
+    config.active_record.schema_format = :sql
   end
 end
 
